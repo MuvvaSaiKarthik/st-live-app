a7d5b0b722ac65fdd97b01671ef948b824355fba
MuvvaSaiKarthik <54205485+MuvvaSaiKarthik@users.noreply.github.com>
2024-03-14 11:53:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 placeholder = st.empty()
 
 while True:
-    df = pd.read_csv('D:\\streamlit_input_files\\pnl_scenario_sqlview\\dropcopy_Dealer_PNL_mar_pinpout_scenario_sqlview.csv')
+    df = pd.read_csv('dropcopy_Dealer_PNL_mar_pinpout_scenario_sqlview.csv')
     placeholder.dataframe(df)
     time.sleep(3)
 
